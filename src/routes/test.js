a8b934b64b55872de2cb0458ab589e3174b2a7b4
Mael Toukap <maeltoukap@gmail.com>
2026-06-21 14:53:47 +0100

    update text

--- src/routes/test.js
+++ src/routes/test.js
@@ -30,7 +30,7 @@ const router = express.Router();
  */
 router.get('/test', (req, res) => {
   res.json({
-    status: 'OK',
+    status: 'OKKK',
     uptime: process.uptime(),
     environment: process.env.NODE_ENV || 'development',
   });
